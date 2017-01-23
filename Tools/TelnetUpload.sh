446f25c22cee21bce619a2b8a6ffeb6d41d0ddff
Dominique DERRIER <derrierdo@gmail.com>
2017-01-22 19:44:59 -0500

    Add compilation after upload

--- Tools/TelnetUpload.sh
+++ Tools/TelnetUpload.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 FILE=$1
+COMPILE=$2
 
 echo "file.open(\""$FILE"\",\"w\")"
 
-while read line 
+while read line
 do
 sleep 0.5
 rline=$(echo -e "$line" |sed -e 's/\"/\\\"/g')
@@ -12,6 +13,13 @@ echo "file.writeline(\"$rline\")"
 done < $FILE
 echo "file.close()"
 echo ""
+
+if [ ! "x${COMPILE}" = "x" ]
+then
+	echo "node.compile(\"$FILE\")"
+	sleep 1
+	echo "file.remove(\"$FILE\")"
+fi
 
 
 ### | socat - tcp4:<IP>:2323
